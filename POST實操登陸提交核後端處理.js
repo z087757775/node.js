2470bf130c9061ef0a3b9ac2d52823bbb6f38584
yung <ching11280714@gmail.com>
2023-07-04 16:08:55 +0800

    POST實操登陸提交核後端處理

--- POST實操登陸提交核後端處理.js
+++ POST實操登陸提交核後端處理.js
@@ -1,5 +1,7 @@
 const http = require('http')
 const fs = require('fs')
+// querystring 將URL其中出現的(sername=666&password=666)這串東西轉換為對象
+const qs = require('querystring')
 // 自創端口 (0~1023號是留給知名端口的範圍 1024號以上可以自創)
 const port = 3000;
 const ip = '192.168.0.103'
@@ -31,8 +33,6 @@ const server = http.createServer((request, response ) => {
     if(method === 'GET') {
         // 使用JS的url來讀取用戶的網址 有兩個參數1.當前訪問的頁面 2.自己的的url(http+ip+端口)
         const requestUrl = new URL(url, `http://${ip}:${port}`);
-        console.log(re);
-        console.log(requestUrl);
         // 這代表伺服器的url地址(沒有包括用戶輸入的參數)
         url = requestUrl.pathname
         // 定義一個獲取用戶輸入的參數裡的值         lang是參數獲取lang他的值
@@ -53,12 +53,50 @@ const server = http.createServer((request, response ) => {
             sendResponse(`index${selector}.html`,200,response);
         }else if(url === '/about.html'){
             sendResponse(`about${selector}.html`,200,response);
+        }else if(url === '/login.html'){
+            sendResponse(`login${selector}.html`,200,response);
+        }else if(url === '/login-success.html'){
+            sendResponse(`login-success${selector}.html`,200,response);
+        }else if(url === '/login-fail.html'){
+            sendResponse(`login-fail${selector}.html`,200,response);
         }else{
             sendResponse(`404${selector}.html`,404,response);
         }
 
+    
+    // 這個else處理 POST的語句
     }else{
-
+     if(url === '/process-login') {
+    // 把前端傳過來的數據 用一個空數組接收(緩衝區)，等待被讀取使用
+    let body = [];
+    // 監聽器(監聽前端發過來的請求)有兩個參數1.要監聽甚麼 2.回調函數
+    request.on('data' ,(chunk) => {
+        // 監聽到函數的參數推到數組裡
+        body.push(chunk)
+    })
+    // 第二個監聽器
+    request.on('end', ()=> {
+        // Buffer.concat() 是將緩衝區零散(Buffer)的數組 合併成一個大的緩區區(Buffer.concat())對象
+        body = Buffer.concat(body).toString()
+        // 拆分(sername=666&password=666) 變為對象
+       body =  qs.parse(body)
+        console.log(body);
+        // 驗證帳號密碼階段 if else
+        if(body.username === 'john' && body.password === 'john123') {
+            // 狀態碼為301意思是 瀏覽器自動導向新的url，連結新的網址url
+            response.statusCode = 301
+            // 指定的網址路線 設置第一個參數Location 是為了告訴瀏覽器我要去指定的url
+            response.setHeader('Location' , '/login-success.html' )
+        }else {
+             // 狀態碼為301意思是 瀏覽器自動導向新的url，連結新的網址url
+             response.statusCode = 301
+             // 指定的網址路線 設置第一個參數Location 是為了告訴瀏覽器我要去指定的url
+             response.setHeader('Location' , '/login-fail.html' )
+        }
+        // 這句話代表回應了客戶的請求並結束了，下面再有句子就會引發錯誤
+        response.end()
+    })
+     }
     }
 })
 
